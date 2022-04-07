8d0903ee47e2aaf29108acb50d90a8aa6c6f028e
Armando Padilla <apadilla@altimetrik.local>
2022-04-06 20:04:29 -0500

    Set variables and create VPC and subnet

--- main.tf
+++ main.tf
@@ -1,39 +1,44 @@
 provider "aws" {
-  region = "us-east-1"
-  
+  region = "us-east-1"  
 }
 
 #simple wat to define a var in tf
-variable "subnect_cidr_block" {
-  description = "subent cidr block"
-  value = "10.0.10.0/24"
-}
+# variable "subnect_cidr_block" {
+#   description = "subent cidr block"
+#   value = "10.0.10.0/24"
+# }
 
-resource "aws_vpc" "altm-vpc" {
+variable "avail_zone" {}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "env_prefix" {}
+
+resource "aws_vpc" "app-vpc" {
   tags = {
-    Name = "alt-vpc"
+    Name = "${var.env_prefix}-vpc"
+    description = "VPC for API users"
   }
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr_block
 }
 
 #each subnet inside a VPC has to have a different set of IP address
-resource "aws_subnet" "altm-subnet" { 
+resource "aws_subnet" "app-subnet-1" { 
   tags = {
-    Name = "altm-subnet"
+    Name = "${var.env_prefix}-subnet-1"
   }
-  vpc_id = aws_vpc.altm-vpc.id
-  cidr_block = var.subnect_cidr_block.value
-  availability_zone = "us-east-1a"
+  vpc_id = aws_vpc.app-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
 }
 
 #Get an existing VPC 
 
-data "aws_vpc" "default_vpc" {
-  tags = {
-    Name = "default-vpc"
-  }
-  default = true
-}
+# data "aws_vpc" "default_vpc" {
+#   tags = {
+#     Name = "default-vpc"
+#   }
+#   default = true
+# }
 
 # resource "aws_subnet" "altm-subnet-default-vpc" {
 #   vpc_id = data.aws_vpc.default_vpc.id
@@ -42,8 +47,8 @@ data "aws_vpc" "default_vpc" {
 # }
 
 #chech outpust of resources
-output "default-vpc-id" {
-  value = data.aws_vpc.default_vpc.id
-}
+# output "default-vpc-id" {
+#   value = data.aws_vpc.default_vpc.id
+# }
 
 
